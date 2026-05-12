19a2fdb0512f1a553e560b26ee7aeed90061a13f
antoniusjairus4 <antoniusjairusofficial@gmail.com>
2026-05-13 03:40:53 +0530

    Tournaments Tracking programme

--- tournament_manager.cpp
+++ tournament_manager.cpp
@@ -128,3 +128,216 @@ class Athlete : public Player {
         cout << "Score : " << calculate_score() << endl;
     }
 };
+
+int main()
+{
+    Table_Tennis tt[MAX];
+    Chess_Player chess[MAX];
+    Athlete ath[MAX];
+
+    int tt_count = 0;
+    int chess_count = 0;
+    int athlete_count = 0;
+
+    int choice;
+
+    do
+    {
+        cout << endl << "Choices are being displayed below" << endl;
+        cout << "1. Add Table Tennis Player" << endl;
+        cout << "2. Add Chess Player" << endl;
+        cout << "3. Add Athlete" << endl; 
+        cout << "4. Display All Players" << endl;
+        cout << "5. Show Top Performer" << endl;
+        cout << "6. Exit" << endl;
+
+        cout << endl << "Enter Choice : ";
+        cin >> choice;
+
+        if (choice == 1)
+        {
+            int id, age;
+            string name;
+
+            int matches_won;
+            float reaction_speed;
+
+            cout << endl << "Enter Player ID : ";
+            cin >> id;
+
+            cin.ignore();
+
+            cout << "Enter Name : ";
+            getline(cin, name);
+
+            cout << "Enter Age : ";
+            cin >> age;
+
+            cout << "Matches Won : ";
+            cin >> matches_won;
+
+            cout << "Reaction Speed : ";
+            cin >> reaction_speed;
+
+            tt[tt_count] = Table_Tennis(id, name, age, matches_won, reaction_speed );
+
+            tt_count++;
+
+            cout << endl << "Player Added Successfully!" << endl;
+        }
+
+        else if (choice == 2)
+        {
+            int id, age;
+            string name;
+
+            int ratings;
+            int tournaments;
+
+            cout << endl << "Enter Player ID : ";
+            cin >> id;
+
+            cin.ignore();
+
+            cout << "Enter Name : ";
+            getline(cin, name);
+
+            cout << "Enter Age : ";
+            cin >> age;
+
+            cout << "Enter Rating : ";
+            cin >> ratings;
+
+            cout << "Enter Tournaments Won : ";
+            cin >> tournaments;
+
+            chess[chess_count] = Chess_Player( id, name, age, ratings, tournaments);
+
+            chess_count++;
+
+            cout << "\nPlayer Added Successfully!" << endl;
+        }
+
+
+        else if (choice == 3)
+        {
+            int id, age;
+            string name;
+
+            float speed;
+            float stamina;
+
+            cout << endl << "Enter Player ID : ";
+            cin >> id;
+
+            cin.ignore();
+
+            cout << "Enter Name : ";
+            getline(cin, name);
+
+            cout << "Enter Age : ";
+            cin >> age;
+
+            cout << "Enter Speed : ";
+            cin >> speed;
+
+            cout << "Enter Stamina : ";
+            cin >> stamina;
+
+            ath[athlete_count] = Athlete(id, name, age, speed, stamina);
+
+            athlete_count++;
+
+            cout << endl << "Player Added Successfully!" << endl;
+        }
+
+        else if (choice == 4)
+        {
+            cout << "\n======= TABLE TENNIS PLAYERS =======\n";
+
+            for (int i = 0; i < tt_count; i++){
+                tt[i].display_details();
+
+                cout << "--------------------------------\n";
+            }
+
+            cout << "\n=========== CHESS PLAYERS ==========\n";
+
+            for (int i = 0; i < chess_count; i++){
+                chess[i].display_details();
+
+                cout << "--------------------------------\n";
+            }
+
+            cout << "\n============= ATHLETES =============\n";
+
+            for (int i = 0; i < athlete_count; i++){
+                ath[i].display_details();
+
+                cout << "--------------------------------\n";
+            }
+        }
+
+        else if (choice == 5)
+        {
+            double max_score = -1;
+            string top_sport;
+
+            for (int i = 0; i < tt_count; i++){
+                if (tt[i].calculate_score() > max_score)
+                {
+                    max_score =
+                        tt[i].calculate_score();
+
+                    top_sport = "Table Tennis";
+                }
+            }
+
+            for (int i = 0; i < chess_count; i++){
+                if (chess[i].calculate_score()
+                    > max_score)
+                {
+                    max_score =
+                        chess[i].calculate_score();
+
+                    top_sport = "Chess";
+                }
+            }
+
+            for (int i = 0; i < athlete_count; i++){
+                if (ath[i].calculate_score()
+                    > max_score)
+                {
+                    max_score =
+                        ath[i].calculate_score();
+
+                    top_sport = "Athlete";
+                }
+            }
+
+            if (max_score == -1) {
+                cout << "\nNo Players Available!\n";
+            }
+            else {
+                cout << "\n========== TOP PERFORMER ==========\n";
+
+                cout << "Sport Type : "
+                     << top_sport << endl;
+
+                cout << "Highest Score : "
+                     << max_score << endl;
+            }
+        }
+
+        else if (choice == 6){
+            cout << "\nExiting Program...\n";
+        }
+
+        else{
+            cout << "\nInvalid Choice!\n";
+        }
+
+    } while (choice != 6);
+
+    return 0;
+}
