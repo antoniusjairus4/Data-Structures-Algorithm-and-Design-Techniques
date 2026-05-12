44b2abd05d85a36ce73a3b4ad826778ed7131d3c
antoniusjairus4 <antoniusjairusofficial@gmail.com>
2026-05-13 02:53:59 +0530

    Tournaments Tracking programme

--- tournament_manager.cpp
+++ tournament_manager.cpp
@@ -19,6 +19,36 @@ class Player{
         }
 
         Player(int id, string n, int a){
-            
+            player_id = id;
+            name = n;
+            age = a;
+        }
+
+        void display_basics(){
+            cout << endl << "Player ID : " << player_id << endl;
+            cout << "Name      : " << name << endl;
+            cout << "Age       : " << age << endl;
+        }
+
+        int get_id(){
+            return player_id;
+        }
+};
+
+class Table_Tennis : public Player{
+    private:
+        int matches_won;
+        float reaction_speed;
+
+    public:
+        Table_Tennis(){
+            matches_won = 0;
+            reaction_speed = 0.0;
+        }
+
+        Table_Tennis(int id, string n, int a, int m, float r) : Player(id, n, a)
+        {
+            matches_won = m;
+            reaction_speed = r;
         }
 };
