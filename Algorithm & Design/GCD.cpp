80517d83c52b0b10efe71604d68cc67e9722e1f0
antoniusjairus4 <antoniusjairusofficial@gmail.com>
2026-06-10 08:41:29 +0530

    GCD program

--- Algorithm & Design/GCD.cpp
+++ Algorithm & Design/GCD.cpp
@@ -2,8 +2,27 @@
 
 using namespace std;
 
-class GCD {
+class GCD_Modulo{
     
+    public:
+        int num_1, num_2;
+
+        int calculate() {
+            int a = num_1;
+            int b = num_2;
+
+            if(a <= 0 || b <= 0){
+                return -1;
+            }
+
+            while (b != 0){
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+        }
+    
+
 };
 
 int main()
