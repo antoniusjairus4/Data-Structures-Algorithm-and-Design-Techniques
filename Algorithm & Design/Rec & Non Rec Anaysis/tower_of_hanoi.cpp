8b32b6fd88808a368385c6bfbf6216d49ad7a993
antoniusjairus4 <antoniusjairusofficial@gmail.com>
2026-06-13 10:02:12 +0530

    Tower of Hanoi

---
+++ Algorithm & Design/Rec & Non Rec Anaysis/tower_of_hanoi.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void tower_hanoi(int n, char s, char a, char d){
+    if(n == 1){
+        cout << "Move disk " << n << " from " << s << " to " << d << endl;
+        return;
+    }
+
+    tower_hanoi(n - 1, s, d, a);
+    cout << "Move disk " << n << " from " << s << " to " << d << endl;
+
+    tower_hanoi(n - 1, a, s, d);
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the number of elements in the Tower: ";
+    if(!(cin >> n) || n < 1){
+        cout << "Invalid input";
+        return 0;
+    }
+
+    tower_hanoi(n, 'A', 'B', 'C');
+}
