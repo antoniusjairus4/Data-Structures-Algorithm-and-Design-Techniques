7012e43c7920d3a52d59fe39954db208106241f8
antoniusjairus4 <antoniusjairusofficial@gmail.com>
2026-06-22 23:45:56 +0530

    The classic rat and the food question

--- Interview_Questions/rats_food.cpp
+++ Interview_Questions/rats_food.cpp
@@ -5,5 +5,22 @@ using namespace std;
 
 int main()
 {
+    int total_rats, food_rat, total_houses;
+    if(!(cin >> total_rats >> food_rat >> total_houses)){
+        cout << "Invalid input" << endl;
+        return 0;
+    }
+
+    if(total_houses < 0 || food_rat < 0 || total_houses){
+        cout << "Invalid input" << endl;
+        return 0;
+    }
+
+    int total_food = total_rats * food_rat;
+    
+    int food_collected = 0;
+    int house_visited = 0;
+
+    
     return 0;
 }
