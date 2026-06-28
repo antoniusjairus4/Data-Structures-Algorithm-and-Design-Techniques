e6dacdc34776a35870552248cf6728c08063417f
antoniusjairus4 <antoniusjairusofficial@gmail.com>
2026-06-28 12:55:15 +0530

    Done with the building question

--- Interview_Questions/My prep/building.cpp
+++ Interview_Questions/My prep/building.cpp
@@ -47,6 +47,20 @@ int main()
 
     int arr[n];
     for(int i = 0; i < n; i++){
-      cin >> arr[n];`
+      cin >> arr[n];
     }
+
+    int min = arr[0];
+
+    int count = 0;
+    for(int i = 0; i < n; i++){
+      
+      if(arr[i] < min){
+        min = arr[i];
+        count++;
+      }
+    }
+
+    cout << count;
+    return 0;
 }
