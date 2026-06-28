f8b65d273d954b482bfd2127bac22d41c46938d0
antoniusjairus4 <antoniusjairusofficial@gmail.com>
2026-06-28 12:51:01 +0530

    Solving the building question

--- Interview_Questions/My prep/building.cpp
+++ Interview_Questions/My prep/building.cpp
@@ -40,5 +40,13 @@ using namespace std;
 int main()
 {
     int n;
-    
+    if(!(cin >> n) || n <= 0 || n > 100){
+      cout << "Invalid input" << endl;
+      return 0;
+    }
+
+    int arr[n];
+    for(int i = 0; i < n; i++){
+      cin >> arr[n];`
+    }
 }
